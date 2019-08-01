721cddc36400d5bddc64f005d9d4213e791d7be6
Joel Richardson <joel.richardson@jax.org>
2019-08-01 14:34:24 -0400

    Allow strains to be commented out.

--- bin/buildMouseDataSet.sh
+++ bin/buildMouseDataSet.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 source utils.sh
 
 STRAINS="strains.tsv"
@@ -6,8 +8,10 @@ RELEASE=97
 while read -u 10 p; do
   IFS='   ' read -r -a array <<< "${p}"
   spath="${array[0]}" 
-  sname="${array[1]}" 
-  staxon="${array[2]}" 
-  ./getGenomeFromEnsembl.sh -r ${RELEASE} -g ${spath} -n ${sname} -x ${staxon} $*
-  checkExit
+  if [[ ${spath} != "#"* ]] ; then
+    sname="${array[1]}" 
+    staxon="${array[2]}" 
+    ./getGenomeFromEnsembl.sh -r ${RELEASE} -g ${spath} -n ${sname} -x ${staxon} $*
+    checkExit
+  fi
 done 10<${STRAINS}
