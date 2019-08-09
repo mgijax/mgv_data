744d90088f2b2499fd48f8628c4da7f6f4a5c5c2
Joel Richardson <joel.richardson@jax.org>
2019-08-09 16:14:46 -0400

    Improved installability, commit 2.

---
+++ bin/config.sh
@@ -0,0 +1,28 @@
+
+PYTHON="python2.7"
+GENOMES_FILE="genomes.tsv"
+
+# downloads directory
+DDIR="./downloads"
+# output directory
+ODIR="./output"
+# web directory
+WDIR="./output"
+
+ENSEMBL_BASE="rsync://ftp.ensembl.org/ensembl/pub"
+
+MGI_MODELS="false"
+MGI_URL="http://www.informatics.jax.org/downloads/mgigff3/MGI.gff3.gz"
+
+ORGANISM=""
+NAME=""
+TAXONID=""
+RELEASE=""
+DRY_RUN=""
+CHR_REGEX="..?"  # matches chromosomes of one or 2 characters
+EXCLUDE_TYPES="biological_region,chromosome,scaffold"
+K="4000000"
+
+DATATYPE="" # models, assembly. If not specified, run all parts
+PHASE="" # download, import. If not specified, run all phases
+
