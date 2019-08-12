cf154c4fcc9e1ed1df2d8ec7a734d96f4d0975f1
Joel Richardson <joel.richardson@jax.org>
2019-08-12 08:55:44 -0400

    Change script first line to be more portable.

--- bin/config.sh
+++ bin/config.sh
@@ -5,14 +5,16 @@ DDIR="./downloads"
 ODIR="./output"
 # web directory (can be the same as the output directory)
 WDIR="./output"
-
-# 
+# first part of url for downloading Ensembl genomes data
 ENSEMBL_BASE="rsync://ftp.ensembl.org/ensembl/pub"
-
-
-CHR_REGEX="..?"  # matches chromosomes of one or 2 characters
+# Regular expression for matching chromosomes
+# Default matches chromosomes of one or 2 characters
+# (good for screening out scaffolds)
+CHR_REGEX="..?" 
+# feature types to exclude
 EXCLUDE_TYPES="biological_region,chromosome,scaffold"
+# transcript file chunk size
 K="4000000"
-#
+# Python executable
 PYTHON="python2.7"
 
