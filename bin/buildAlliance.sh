fea1cbdc146d4d5e454a75640658982a41447b43
Joel Richardson <joel.richardson@jax.org>
2019-11-01 14:46:10 -0400

    build-alliance, first commit

---
+++ bin/buildAlliance.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# buildAlliance.sh
+#
+
+source config.sh
+source utils.sh
+#
+./getHomologies.sh 10090 10116 9606 7955 6239 7227 559292
+#
+./getGenome.sh -x 10090 -m MGI,tagEnsemblWithHid        -g mus_musculus            -n M.musculus --gff-url "${MGI_URL}" $*
+./getGenome.sh -x 9096  -m ensembl,stripPrefix,tagEnsemblHuman,tagEnsemblWithHid -g homo_sapiens     -n H.sapiens $*
+./getGenome.sh -x 10116 -m ensembl,stripPrefix,tagEnsemblRat,tagEnsemblWithHid  -g rattus_norvegicus -n R.norvegicus $*
+./getGenome.sh -x 7955  -m ensembl,stripPrefix,tagEnsemblWithHid  -g danio_rerio -n D.rerio $*
+./getGenome.sh -x 6239  -m ensembl,stripPrefix,tagEnsemblWithHid  -g caenorhabditis_elegans -n C.elegans $*
+./getGenome.sh -x 7227 -m ensembl,stripPrefix,tagEnsemblWithHid  -g drosophila_melanogaster -n D.melanogaster $*
+
+# fixme: yeast name should include "S288C"
+./getGenome.sh -x 559292 -m ensembl,stripPrefix,tagEnsemblWithHid  -g saccharomyces_cerevisiae -n S.cerevisiae $*
+
