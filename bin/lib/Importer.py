eeafaffa9dae0ada4ed5566c9b8becde428bd7f0
Joel Richardson <joel.richardson@jax.org>
2020-08-19 16:08:34 -0400

    Merge the old GffImporter into the new Importer framework.

--- bin/lib/Importer.py
+++ bin/lib/Importer.py
@@ -64,12 +64,11 @@ class FastaImporter (Importer) :
             self.builder.ensureDirectory(odir, empty=True)
             split(self.filterDownloadedFile(), odir, self.chr_re, lfunc)
 
-from .GffImporter import Gff3Importer, getArgs
 class GffImporter (Importer) :
     def __init__ (self, *args):
         Importer.__init__(self, *args)
         self.doSort = self.cfg[self.type].get("doSort", False)
-        self.transcriptChunkSize = self.cfg[self.type]["transcriptChunkSize"]
+        self.transcriptChunkSize = int(self.cfg[self.type]["transcriptChunkSize"])
 
     def streamDownloadedFile (self) :
         # GffImporter streams the file a gene model at a time.
@@ -86,8 +85,7 @@ class GffImporter (Importer) :
             gffStream = gp.sortIterate()
         else:
             gffStream = gp.iterate()
-        for x in gffStream:
-            yield x
+        return gffStream
 
     def filterDownloadedFile (self) :
         for obj in Importer.filterDownloadedFile(self):
@@ -96,28 +94,147 @@ class GffImporter (Importer) :
                 yield obj
             else:
                 # yield features on matching chromosomes
-                for f in obj:
-                    if self.chr_re.match(f[0]):
-                        yield formatLine(f)
+                if len(obj):
+                    if self.chr_re.match(obj[0][0]):
+                        yield obj
+
+    def getFileName(self, track, chr, blk):
+        if chr:
+            return os.path.join(self.mOutputDir, track, chr, str(blk) + '.gff3')
+        else:
+            return os.path.join(self.mOutputDir, track, str(blk), '.gff3')
+
+    def getFileHandle(self, fname):
+        if fname == self.currFileName:
+            # currently open file
+            return self.currFile
+        elif fname in self.outputFiles:
+            # different from current but seen before, reopen in append mode
+            self.currFile.close()
+            self.currFileName = fname
+            self.currFile = open(fname, 'a')
+            return self.currFile
+        else:
+            # first time seeing this fname
+            self.builder.ensureDirectory(os.path.dirname(fname))
+            self.currFile and self.currFile.close()
+            self.currFileName = fname
+            self.currFile = open(fname, 'w')
+            self.outputFiles[fname] = 0
+
+            fn = os.path.basename(fname)
+            dn = os.path.basename(os.path.dirname(fname))
+            self.log('%s/%s ' % (dn, fn), newline=' ', timestamp=False)
+            return self.currFile
+
+    def formatFeature(self, f):
+        return formatLine(f)
+
+    def writeGrpToBlk(self, grp, track, chr, blk):
+        fname = self.getFileName(track, chr, blk)
+        fd = self.getFileHandle(fname)
+        for f in grp:
+            if 'exons' in f[8]:
+                fd.write(self.formatFeature(f))
+                self.outputFiles[fname] += 1
+
+    def writeGrp (self, track, grp) :
+        if len(grp) == 0: return
+        chr = grp[0][0]
+        if self.transcriptChunkSize == 0 :
+            self.writeGrpToBlk(grp, track, None, 0)
+        elif self.transcriptChunkSize == 1:
+            self.writeGrpToBlk(grp, track, chr, 0)
+        else:
+            gStart = min([f[3] for f in grp])
+            gEnd = max([f[4] for f in grp])
+            startBlk = gStart // self.transcriptChunkSize
+            endBlk = gEnd // self.transcriptChunkSize
+            for blk in range(startBlk, endBlk+1):
+                self.writeGrpToBlk(grp, track, chr, blk)
+
+    def writeGene(self, f):
+        self.tlFile.write(self.formatFeature(f))
+
+    # Processes a gene model group.
+    # For transcripts, adds an exons attribute whose value is a list 
+    # of exon coordinates. Each exon is encoded as 'offset_length', where offset is equal to
+    # exon.start - transcript.start, and length is equal to exon.end - exon.start + 1.
+    # Also keeps track of all chromosomes seen in the file and the max coordinate seen on each.
+    # Writes genes to genes track and transcripts to transcripts track.
+    def processGrp(self, grp):
+        # Build index from ID to the features that have a Parent attribute with that ID.
+        # Keep track of the chromosome in the file, their order, and the max coordinate of features on each.
+        pid2kids = {}
+        for f in grp:
+          seqid = f[0]
+          c = self.seenChrs.get(seqid, None)
+          if not c:
+            c = { 'name': seqid, 'length': 0 }
+            self.seenChrs[seqid] = c
+            self.seenChrOrder.append(c)
+          c['length'] = max(c['length'], f[4])
+          for pid in f[8].get('Parent',[]):
+            pid2kids.setdefault(pid, []).append(f)
+        # For transcripts, encode exon coordinates and store in exons attribute
+        transcripts = []
+        for f in grp:
+          fid = f[8].get('ID', None)
+          if 'Parent' not in f[8]:
+            # top level feature
+            self.writeGene(f)
+          elif fid in pid2kids:
+            # mid level feature
+            # For efficiency of transfer, encode the exons for a transcript into a col9 attribute.
+            # For each exon, just need its offset from the start of the transcript and its length.
+            # This requires a lot less space (~time) to transfor than representing each exon as a separate
+            # feature with full coordinates. NOTE that we lose exon IDs (and potentially other attributes) 
+            # by doing this. Maybe worth it, maybe not?
+            exons = list([x for x in pid2kids[fid] if x[2] == 'exon'])
+            exons.sort(key=lambda e: e[3])
+            eexons = f[8].setdefault('exons',[])
+            for e in exons:
+              offset = e[3] - f[3]
+              length = e[4] - e[3] + 1
+              eexons.append('%d_%d' % (offset,length))
+            #
+            cdss = list([x for x in pid2kids[fid] if x[2] == 'CDS'])
+            if cdss:
+              cdss.sort(key=lambda x: x[3])
+              attrs = cdss[0][8]
+              cid = attrs['ID']
+              prid = attrs.get('protein_id', attrs.get('curie', ''))
+              cstart = cdss[0][3]
+              cend = cdss[-1][4]
+              f[8]['cds'] = '%s|%s|%d|%d' % (cid, prid, cstart, cend)
+            #
+            transcripts.append(f)
+          else:
+            # leaf level feature
+            pass
+        self.writeGrp('transcripts', transcripts)
 
     def go (self):
-        odir = os.path.join(self.output_dir, self.cfg["name"], "models")
-        gdir = os.path.join(odir, "genes")
-        self.builder.ensureDirectory(gdir, empty=True)
-        tdir = os.path.join(odir, "transcripts")
-        self.builder.ensureDirectory(tdir, empty=True)
-        opts = getArgs([
-            "-p", self.cfg["name"],
-            "-g", self.cfg["label"],
-            "-x", self.cfg["taxonid"],
-            "-d", self.output_dir,
-            "-k", self.transcriptChunkSize,
-        ])
-        self.log("Importing GFF: %s -> %s" % (self.fpath, odir))
+        self.mOutputDir = os.path.join(self.output_dir, self.cfg["name"], "models")
+        self.gOutputDir = os.path.join(self.mOutputDir, "genes")
+        self.tOutputDir = os.path.join(self.mOutputDir, "transcripts")
+        self.builder.ensureDirectory(self.gOutputDir, empty=True)
+        self.builder.ensureDirectory(self.tOutputDir, empty=True)
+        #
+        self.seenChrs = {}
+        self.seenChrOrder = []
+        self.tlFileName = os.path.join(self.gOutputDir, '0.gff3')
+        self.tlFile = open(self.tlFileName, 'w')
+        self.log("Writing genes to: " + self.tlFileName)
+        self.currFileName = None
+        self.currFile = None
+        self.outputFiles = {}
+        #
+        self.log("Importing GFF: %s -> %s" % (self.fpath, self.mOutputDir))
         if not self.debug:
-            imp = Gff3Importer(self.filterDownloadedFile(), opts)
-            imp.main()
-
+            for grp in self.filterDownloadedFile():
+                if type(grp) is not str:
+                    self.processGrp(grp)
 ##
 
 import sys
