f94ec629c1adb323c41d10db2a9d0e012c8b9f39
Joel Richardson <joel.richardson@jax.org>
2022-01-07 14:15:27 -0500

    Fix rat versioning. Rnor 6 ends at Ensemble release 104.

--- bin/compileConfig.sh
+++ bin/compileConfig.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/bash
-
+#
 export SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-
 source ${SCRIPT_DIR}/buildcore.sh
-
 activateVenv
+#
 python ${SCRIPT_DIR}/compileConfig.py $1 > $2
 chmod +x $2
-
-
