714c1c51013f59506b9874d682cc7d66ebd36db8
Joel Richardson <joel.richardson@jax.org>
2022-01-16 06:34:31 -0500

    No more requirement for globally unique ID attrs.

--- bin/lib/MgiGffFilter.py
+++ bin/lib/MgiGffFilter.py
@@ -1,15 +1,5 @@
 from .GffFilter import GffFilter, curie_ize
 class MgiGffFilter (GffFilter) :
-    # To allow build 38 and 39 to coexist in the viewer, need to modify the feature IDs so they're unique.
-    def processModel (self, model) :
-        mid = model[0][8]['ID']
-        newMid = mid + '_' + self.gcfg['build']
-        model[0][8]['ID'] = newMid
-        for f in model[1:]:
-            if mid in f[8].get('Parent', []):
-                f[8]['Parent'] = [newMid]
-        return GffFilter.processModel(self, model)
-
     def processFeature(self, f):
         attrs = f[8]
         if f[2] in ["gene","pseudogene"]:
